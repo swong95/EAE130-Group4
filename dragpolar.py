0759f529ef527faeaea678ca6b959d0573744f9b
swong95 <seawong@ucdavis.edu>
2026-02-02 20:23:07 -0800

    changed equations for drag polar

--- dragpolar.py
+++ dragpolar.py
@@ -18,19 +18,20 @@ cL_clean = np.linspace(-0.9, 0.9, 100)
 cL_takeoff = np.linspace(-2.0, 2.0, 100)
 cL_landing = np.linspace(-2.6, 2.6, 100)
 
-# 1. Clean Configuration: CD = 0.01597 + 0.0406 * CL^2
-clean_drag = 0.01597 + 0.0406 * cL_clean**2
+# Sean - modified drag polors to follow eq C_D = (C_D_0 + Delta_C_D_0) + (1/pi e AR) * C_L^2
 
-# 2. Takeoff Flaps: CD = 0.02597 + 0.0433 * CL^2
-# Note: Using your provided 0.02597 constant
-takeoff_drag = 0.02597 + 0.0433 * cL_takeoff**2
+# 1. Clean Configuration: CD = 0.01597 + 0.0406 * CL^2 (oringinal eq before modifications below)
+clean_drag = C_D_0_calc + (1/np.pi*0.8*AR) * cL_clean**2
 
-# 3. Landing Flaps: CD = 0.07097 + 0.0464 * CL^2
-landing_flaps_drag = 0.07097 + 0.0464 * cL_landing**2
+# 2. Takeoff Flaps: CD = 0.02597 + 0.0433 * CL^2 (oringinal eq before modifications below)
+takeoff_drag = (C_D_0_calc + 0.01) + (1/np.pi*0.75*AR) * cL_takeoff**2
 
-# 4. Landing Gear: CD = 0.03097 + 0.0406 * CL^2
+# 3. Landing Flaps: CD = 0.07097 + 0.0464 * CL^2 (oringinal eq before modifications below)
+landing_flaps_drag = (C_D_0_calc + 0.055) + (1/np.pi*0.75*AR) * cL_landing**2
+
+# 4. Landing Gear: CD = 0.03097 + 0.0406 * CL^2 (oringinal eq before modifications below)
 # (Calculated using the landing CL range)
-landing_gear_drag = 0.03097 + 0.0406 * cL_landing**2
+landing_gear_drag = (C_D_0_calc + 0.015) + (1/np.pi*AR) * cL_landing**2
 
 # --- Plotting ---
 plt.figure(figsize=(10, 6))
@@ -52,3 +53,6 @@ plt.xlim(0, 0.45)
 plt.tight_layout()
 
 plt.show()
+
+# Requirements and Regulations 
+V_approach = 244.732  # ft/s (145 knots)
